7e61fdd23c872c2c910b2c376a509ac3860b9283
Md Shamim <shamimice03@gmail.com>
2023-02-21 15:33:14 +0900

    Update docker-on-amazon-linux-2.sh

--- docker-on-amazon-linux-2.sh
+++ docker-on-amazon-linux-2.sh
@@ -6,9 +6,10 @@ sudo yum -y install docker
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo docker version
+
+# Adding new group
 sudo groupadd docker
+
+# Adding current use to the group
+# so that, it can access docker without sudo
 sudo usermod -aG docker $USER
-
-
-
-
