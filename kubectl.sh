999e80146317f27dc4e23a37193d8fefb156de34
Md Shamim <shamimice03@gmail.com>
2023-02-21 17:10:53 +0900

    Create kubectl.sh

---
+++ kubectl.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+chmod +x kubectl
+mkdir -p ~/.local/bin
+mv ./kubectl ~/.local/bin/kubectl
+kubectl version --client
+
